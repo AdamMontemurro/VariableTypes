ad6bf1071bc66a41ed6a0cb292b7b35e227eb49b
Adam Montemurro <adammontemurro@gmail.com>
2023-04-11 15:20:28 -0400

    Age varible declared and read

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    int age;
+    age = 30;
+
+    System.out.println("I am " + age + " years old.");
   }
 }
